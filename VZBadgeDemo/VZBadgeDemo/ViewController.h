53f2c2c0875101aa8447cdd11f471336e515dab2
victor <victorzhang@vip.163.com>
2015-11-02 19:21:17 +0800

    init project

---
+++ VZBadgeDemo/VZBadgeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VZBadgeDemo
+//
+//  Created by mini4s215 on 11/2/15.
+//  Copyright © 2015 mini4s215. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VZBadgeDemo/VZBadgeDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VZBadgeDemo
+//
+//  Created by mini4s215 on 11/2/15.
+//  Copyright © 2015 mini4s215. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
